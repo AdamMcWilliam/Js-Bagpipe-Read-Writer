fac14393b3e267b75968470e33fe3403b8b6599c
AdamMcWilliam <adammcwilliam@gmail.com>
2020-08-02 00:52:19 -0500

    Updates
    Sheet music canvas update
    Start of note placement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,138 @@
+//Globals
+var noteX = 0;
+
+//resetGlobals
+function resetGlobals(){
+    noteX = 0
+    console.log("noteX: "+noteX);
+    return noteX;
+}
+
+
 //New Button
 function newSheet() {
     var c = document.getElementById("sheetCanvas");
 
-    c.width = c.height *
-        (c.clientWidth / c.clientHeight);
+    c.width = 1500;
+    c.height = 900;
 
     var ctx = c.getContext("2d");
 
-    ctx.lineWidth = 0.8;
+    //clear first
+    ctx.clearRect(0, 0, c.width, c.height);
+    //rest notesX
+    resetGlobals()
+    
+
+    ctx.lineWidth = 2;
 
     //lines
-    var x = 10;
-    var y = 15;
-    var lineLength = 340;
+    //Starting X
+    var x = 25;
+    //Starting Y
+    var y = 120;
+    var lineLength = 1480;
 
-    for (var i = 0; i <= 4; i++) {
+    for (var j = 0; j <= 3; j++) {
 
-        ctx.beginPath();
-        ctx.moveTo(x, y);
-        ctx.lineTo(lineLength, y);
-        ctx.stroke();
-        ctx.closePath();
+        for (var i = 0; i <= 4; i++) {
 
-        y += 10;
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(lineLength, y);
+            ctx.stroke();
+            ctx.closePath();
+
+            //line spacing
+            y += 20;
+        }
+        //spacing between line sections
+        y += 80;
     }
 
     //bars
-    var x = 80
-    var y1 = 15;
-    var y2 = 55;
+    //start at end of line X and work backwards
+    var x = lineLength;
+    var y1 = 120;
+    var y2 = 200;
 
-    for (var i = 0; i <= 4; i++) {
+    for (var j = 0; j <= 3; j++) {
 
-        ctx.beginPath();
-        ctx.moveTo(x, y1);
-        ctx.lineTo(x, y2);
-        ctx.stroke();
-        ctx.closePath();
+        for (var i = 0; i <= 3; i++) {
 
-        x += 86.5;
+            ctx.beginPath();
+            ctx.moveTo(x, y1);
+            ctx.lineTo(x, y2);
+            ctx.stroke();
+            ctx.closePath();
 
+            //space between bars
+            x -= 400;
+
+        }
+        x = lineLength;
+        y1 += 180;
+        y2 += 180;
     }
 
     drawing = new Image();
     drawing.src = "assets/images/Stave.png";
-    drawing.onload = function() {
-        ctx.drawImage(drawing, 10, 15, 15, 35);
+    drawing.onload = function () {
+        ctx.drawImage(drawing, 30, 120, 30, 72);
     }
 
 
+}
+
+
+function getNoteX(){
+    console.log("noteX: "+noteX);
+    return noteX+=80; 
+}
+
+function placeNote(note) {
+
+    var c = document.getElementById("sheetCanvas");
+    var ctx = c.getContext("2d");
+
+    var noteX = getNoteX();
+
+    var noteY;
+
+    switch (note) {
+        case "lowG":
+            noteY = 172;
+            break;
+        case "lowA":
+            noteY = 162;
+            break;
+        case "b":
+            noteY = 152;
+            break;
+        case "c":
+            noteY = 142;
+            break;
+        case "d":
+            noteY = 132;
+            break;
+        case "e":
+            noteY = 122;
+            break;
+        case "f":
+            noteY = 112;
+            break;
+        case "highG":
+            noteY = 102;
+            break;
+        case "highA":
+            noteY = 92;
+            break;
+    }
+
+
+
+
+    note = new Image();
+    note.src = "assets/images/note.png";
+    ctx.drawImage(note, noteX, noteY, 35, 40);
 }
